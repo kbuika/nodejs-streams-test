854c83358c39406c51d155adb20f2d1f15bc0423
kibuikaCodes <kibuikasteve@gmail.com>
2023-10-19 14:33:10 +0300

    add tests and clean-up

--- routes/generate-csv.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-const { generateCSV } = require("../controllers/generate-csv");
-
-router.route("/csv").get(generateCSV);
-
-module.exports = router;
--- routes/generate-mock-data.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-const { generateMockData } = require("../controllers/generate-mock-data");
-
-router.route("/generate-mock-data").post(generateMockData);
-
-module.exports = router;
